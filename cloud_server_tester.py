66db8d7bf96d27519f55f64b242bd56f0ff513c6
DanielBaqaeen <59934946+DanielBaqaeen@users.noreply.github.com>
2021-12-04 17:15:07 -0500

    added arguments for number of concurrent tests and source file

--- cloud_server_tester.py
+++ cloud_server_tester.py
@@ -10,10 +10,11 @@ import argparse
 #number of time argument
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n", type=str)
+parser.add_argument("--n", type=int, required=True)
 parser.add_argument("-source", type=str)
 args = parser.parse_args()
 
+conc_tests = args.n
 
 def random_string(length):
     return ''.join(random.choice(string.ascii_letters) for _ in range(length))
@@ -21,15 +22,18 @@ def random_string(length):
 
 def caller(): 
   UID= random_string(length = 10)
-  File = uuid.uuid4().hex 
-  command = "python ./cloud_client.py -uid " +  UID + " -file " + File + ".txt -data hello.txt"
+  File = uuid.uuid4().hex
+  if args.source is None:
+     command = "python ./cloud_client.py -uid " +  UID + " -file " + File + ".txt"
+  else: 
+     command = "python ./cloud_client.py -uid " +  UID + " -file " + File + ".txt -data " + args.source
   os.system(command)
 
 cpus = int(round(mp.cpu_count() / 2 ))
 
 if __name__ == '__main__':
     p1 = mp.Pool(cpus)
-    for i in range(100):
+    for i in range(conc_tests):
         p1.apply_async(caller)
     p1.close()
     p1.join()
